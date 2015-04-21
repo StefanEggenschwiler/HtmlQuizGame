cb22fa30fea9dac6ad5b33ff586339b2f8539b02
Stefan Eggenschwiler <stefan.eggenschwiler@students.hevs.ch>
2015-04-21 10:27:36 +0200

    Updated Javascript. (Added removeQuestion, refactored createQuestion) Answer is now an internal variable so the user can't "cheat" anymore.

--- LogicScript.js
+++ LogicScript.js
@@ -1,48 +1,54 @@
 /**
  * Created by Stefan on 20-Apr-15.
  */
-var i;
+var questionNo;
+var answer;
 
 function initialize() {
-    i = 1;
-    var answerOptions = [3, 4, 5, 6, 7];
-    var answer = 3;
-    createQuestion("Are you mad, bro?", answerOptions, answer);
-    i++;
+    questionNo = 1;
+    createQuestion("Welche Farbe hat der Himmel?", ["Blau", "Rot", "Grün", "Pink", "Affe"], 0);
 }
 
-function createQuestion(question, answerOptions, answer) {
-    document.getElementById("question").innerHTML = i +'. Question: '+question;
+function createQuestion(question, answerOptions, ans) {
+    document.getElementById("question").innerHTML = questionNo +'. Question: '+question;
+    answer = ans;
 
     for (var index = 0; index < answerOptions.length; index++) {
-        if(index+1 === answer) {
-            makeRadioButton(true, answerOptions[index]);
-        } else {
-            makeRadioButton(false, answerOptions[index]);
-        }
+        createRadioButton(answerOptions[index]);
     }
+    questionNo++;
 }
 
 function evaluateQuestion() {
     var radios = document.getElementsByName("answerOptions");
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
-            if(radios[i].value === "true"){
-                alert('Answer is true!');
+            if(i == answer){
+                alert('Answer is true! Next Question!');
+                removePreviousQuestion();
+                createQuestion("Wieviele Sterne hat das Walliser Wappen?", [10, 12, 13, 19, 22, "over 9000"], 2);
             } else {
-                alert('Answer is false! Value: '+radios[i].value);
+                alert('Answer is false! Try again!');
             }
         }
     }
 }
 
+//http://stackoverflow.com/a/3955238
+function removePreviousQuestion() {
+    var myNode = document.getElementById("placeholder");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+    }
+}
+
 //http://stackoverflow.com/a/23430717
-function makeRadioButton(value, text) {
+function createRadioButton(text) {
     var label = document.createElement("label");
     var radio = document.createElement("input");
     radio.type = "radio";
     radio.name = "answerOptions";
-    radio.value = value;
+    //radio.value = value;
     label.appendChild(radio);
     label.appendChild(document.createTextNode(text));
     label.appendChild(document.createElement("br"));
